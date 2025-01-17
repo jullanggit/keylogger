c461b0f03e295cabb5de7f09298d9923c31cd25d
Jul Lang <jullanggit@proton.me>
2025-01-17 11:34:24 +0100

    switch to evdev-rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::fs;
 
-use evdev::{Device, InputEventKind, Key};
+use evdev_rs::{
+    Device, DeviceWrapper, InputEvent, ReadFlag,
+    enums::{EV_KEY::*, EventCode},
+};
 
 fn main() {
     let inputs = fs::read_dir("/dev/input").unwrap();
@@ -9,7 +12,7 @@ fn main() {
         .filter_map(|entry| entry.ok())
         .map(|entry| entry.path())
         .find(|path| {
-            if let Ok(device) = Device::open(path) {
+            if let Ok(device) = Device::new_from_path(path) {
                 if let Some(name) = device.name() {
                     name.contains("evremap")
                 } else {
@@ -21,26 +24,30 @@ fn main() {
         })
         .expect("Failed to find keyboard input device");
 
-    let mut keyboard = Device::open(keyboard_path).unwrap();
+    let mut keyboard = Device::new_from_path(keyboard_path).unwrap();
     println!(
         "Listening for key events on: {}",
         keyboard.name().unwrap_or("Unknown Device")
     );
 
     loop {
-        for ev in keyboard.fetch_events().unwrap() {
-            if let InputEventKind::Key(key) = ev.kind() {
-                if ev.value() == 1 {}
-            }
+        let event = keyboard
+            .next_event(ReadFlag::NORMAL)
+            .map(|(_status, event)| event);
+
+        match event {
+            Ok(event) => if let EventCode::EV_KEY(key) = event.event_code {},
+
+            Err(e) => eprintln!("{e}"),
         }
     }
 }
 
-fn to_letter(key: Key) -> char {
-    match key {
-        _ => {}
-    }
-    todo!()
-}
+// fn to_letter(key: Key) -> char {
+//     match key {
+//         _ => {}
+//     }
+//     todo!()
+// }
 
 struct Log {}
